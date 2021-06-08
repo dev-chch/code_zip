b4e6918d611986f28398e01712fe8fe653766150
PROJECT0210 <65739125+project0210@users.noreply.github.com>
2021-06-08 21:30:17 +0900

    Update and rename how to use add_shortcode.php to add_shortcode의 활용법.php

--- add_shortcode의 활용법.php
+++ add_shortcode의 활용법.php
@@ -11,20 +11,26 @@ function project0210_baztag_func(){
 
 //감싸는 형태 [baztag]content[/baztag]
 add_shortcode('baztag', 'project0210_baztag_func');
-function project0210_baztag_func( $atts, $content = "" ) {
+function project0210_baztag_func($atts, $content = ""){
     return $content;
 }
   
-//API 사용 간단한 형태 [footag foo="bar"]
+//인자 사용 [footag foo="bar"]
 add_shortcode('footag', 'project0210_footag_func');
-function project0210_footag_func( $atts ) {
+function project0210_footag_func($atts){
     return {$atts['foo']};
+}
+
+//인자+감싸는 형태 [baztag att='']content[/baztag]
+add_shortcode('baztag', 'project0210_baztag_func');
+function project0210_baztag_func($atts, $content = ""){
+    return $atts['att'], $content;
 }
 
 //API 기본 형태 [bartag foo="bar"]
 add_shortcode('bartag', 'project0210_bartag_func');
-function project0210_bartag_func( $atts ) {
-    $atts = shortcode_atts( array(
+function project0210_bartag_func($atts){
+    $atts = shortcode_atts(array(
         'foo' => 'no foo',
         'baz' => 'default baz'
     ), $atts, 'bartag' );
