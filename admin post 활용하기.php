6abad6b82c1f0cc5215032e116357fb945e5b19e
PROJECT0210 <65739125+project0210@users.noreply.github.com>
2021-08-23 10:41:54 +0900

    Create admin post 활용하기.php

---
+++ admin post 활용하기.php
@@ -0,0 +1,11 @@
+<input name='action' type="hidden" value='custom_admin_form_submit'>
+<!-- form을 보낼 때 action value 입력 -->
+
+<?php
+add_action('admin_post_custom_admin_form_submit','custom_admin_form_submit_function');
+// admin_post_(action value)로 구분해서 동작 처리
+function custom_admin_form_submit_function(){
+   print_r($_POST);
+   wp_redirect(get_permalink('page_id')));
+   exit;
+}
