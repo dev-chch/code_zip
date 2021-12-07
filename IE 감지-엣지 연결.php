a08f1d9c38c7e5f7931bc9478bbfb152434ccb60
PROJECT0210 <65739125+project0210@users.noreply.github.com>
2021-12-08 00:11:05 +0900

    21-12-08

--- IE 감지-엣지 연결.php
+++ IE 감지-엣지 연결.php
@@ -1,6 +1,9 @@
 <?php //php
-if (preg_match('/MSIE|Internet Explorer|Trident/i', $_SERVER['HTTP_USER_AGENT'])) {
-    echo '<script>window.location="microsoft-edge:https://marshall-ku.com'.$_SERVER['REQUEST_URI'].'";</script>';
+add_action('init', 'wpboard_is_ie');
+function wpboard_is_ie(){
+	if(preg_match('/MSIE|Internet Explorer|Trident/i', $_SERVER['HTTP_USER_AGENT'])){
+		echo '<script>window.location="microsoft-edge:'.get_permalink().$_SERVER['REQUEST_URI'].'";</script>';
+	}
 }
 ?>
 
